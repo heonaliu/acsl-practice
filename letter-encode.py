265f3f0dca04afbd6a008678f8e2e65333ae3f9d
Heona Liu <heonaliu@gmail.com>
2025-12-23 17:24:17 -0500

    complete wrap around code - acsl practice

---
+++ letter-encode.py
@@ -0,0 +1,103 @@
+#2001 - 2002 ACSL Wrap Around Code
+#Heona Liu 12/23/25
+
+def get_alpha_pos(char):
+    if not char.isalpha() or len(char) != 1:
+        return "Invalid Character"
+    pos = ord(char.lower()) - ord('a') + 1
+    return int(pos)
+
+def to_letter(num):
+    return chr(ord("A") + num - 1)
+
+def largest_factor(n):
+    for i in range(n//2, 0, -1):
+        if n%i == 0:
+            return i  
+    return 1
+
+def getMethod(char):
+    m1 = "ABCDE"
+    m2 = "FGHIJ"
+    m3 =  "KLMNO"
+    m4 = "PQRST"
+    m5 = "UVWXYZ"
+    
+    if char in m1:
+        return 1
+    if char in m2:
+        return 2
+    if char in m3:
+        return 3
+    if char in m4:
+        return 4
+    if char in m5:
+        return 5
+    
+    #example usage: (1, 2, 1) --> (A, B, 1) returns 5
+    #example usage: (25, 2, 1) --> (Y, B, 1) 
+def new_character(prev, curr, method):
+    if (method == 1): #multiply by 2
+        #print("METHOD 1: MULTIPLY BY 2")
+        curr*=2
+        new = int((prev+curr))%26
+        return to_letter(new)
+    elif(method == 2): #divide num by 3, multiply rem by 5
+        #print("METHOD 2: DIVIDE NUM BY 3 MULTIPLY REM BY 5")
+        new = int(curr % 3) * 5
+        new = int((prev+new))%26
+        return to_letter(new)
+        
+    elif(method == 3): #divide num by 4, multiply int of quot by 8
+        #print("METHOD 3: DIVIDE NUM BY 4 MULTIPLY INT BY 8")
+        new = int(curr/4) * 8
+        new = int((prev+new))%26
+        return to_letter(new)
+    
+    elif(method == 4): #mult. sum of digits by 10 
+        ##### NEEDS FIXXING
+        #print("METHOD 4: MULT SUM OF DIGITS BY 10")
+        sum =0
+        #count the number of digits
+        while curr > 0:
+            digit=curr%10
+            sum += digit
+            curr = int(curr/10)
+        new = (sum * 10)%26
+        new = int((prev+new))%26
+        return to_letter(new)
+    elif(method == 5): #find largest factor then mult. by. 12
+        #print("METHOD 5: FIND LARGEST FACTOR THEN MULT. BY 12")
+        new = int(largest_factor(curr) * 12) % 26
+        new = int((prev+new))%26
+        return to_letter(new) 
+        
+    else:
+        return " - ELSE"
+
+inputs = []
+
+prev = ""
+for i in range(5):
+    sample = input(str(i+1) + ". ")
+    sample = sample.replace(" ", "")
+    w = sample.split(",")
+    inputs.append(w)
+    
+#print(inputs)
+for i in range(len(inputs)):
+    word = ""
+    for j in range(len(inputs[i])-1):
+        letter = inputs[i][j]
+        alpha_pos = get_alpha_pos(letter)
+        if prev == "": #means it's the first letter
+            prev = 1 #set it to A
+        #print(letter + " pos --> " + str(alpha_pos))
+        converted = new_character(prev, alpha_pos, getMethod(letter))
+        #print("...."+converted)
+        word += " " +converted
+        #reassign previous letter to position
+        prev = get_alpha_pos(converted)
+    
+    print(str(i+1) + ". " + word)    
+    prev = ""
